5e412f82a893ba191356090a991c71d31238f585
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2022-11-15 15:22:58 -0400

    utilizando unform

--- src/pages/Sigup/index.tsx
+++ src/pages/Sigup/index.tsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
+import { Form } from '@unform/web'
 import { Container, Content, Background } from './styles';
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
-const SigUp: React.FC = () => (
-  <Container>
+const SigUp: React.FC = () => {
+  const handleSubmit = (data:object) => {
+     
+  }
+
+  return (
+    <Container>
     <Background />
     <Content>
       <img src={logoImg} />
-      <form>
+      <Form onSubmit={handleSubmit}>
         <h2>Faça seu cadastro</h2>
         <Input name="nome" icon={FiUser} type="text" placeholder='Nome' />
         <Input name="email" icon={FiMail} type="text" placeholder='E-mail' />
         <Input name="password" icon={FiLock} type="password" placeholder='Senha' />
         <Button type="submit">Cadastrar</Button>
 
-      </form>
+      </Form>
       
       <a href="#"><FiArrowLeft /> Voltar logon</a>
     </Content>
     
   </Container>
-)
+  )
+}
 
 export default SigUp;
