15f763966e68b61cedc53e9b27b7686a2df3232c
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2023-01-02 16:07:37 -0400

    Estrutura de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+
 import SigIn from './pages/Sigin';
 import SigUp from './pages/Sigup';
+
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => {
@@ -11,6 +14,8 @@ const App: React.FC = () => {
       <AuthProvider>
         <SigIn />
       </AuthProvider>
+      
+      <ToastContainer/>
       <GlobalStyle />
     </>
   );
