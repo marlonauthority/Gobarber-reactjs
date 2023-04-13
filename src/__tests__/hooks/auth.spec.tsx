4f2524e4932481597962bbcf3d340e1ceb8bcd7e
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2023-04-13 14:25:16 -0400

    Criando mock da API

--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -1,10 +1,23 @@
-import { renderHook } from '@testing-library/react-hooks';
-
+import { renderHook, act } from '@testing-library/react-hooks';
+import MockAdapter from 'axios-mock-adapter';
 import { AuthProvider, useAuth } from '../../hooks/AuthContext';
+import api from '../../utils/apiClient'
+
+const apiMock = new MockAdapter(api);
+
 
 describe('Auth hook', () => {
+  apiMock.onPost('sessions').reply(200, {
+    user: {
+      id: 'user-123',
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    },
+    token: 'token-123',
+  });
+
   it('pode logar', async () => {
-    const { result } = renderHook(() => useAuth(), {
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
 
@@ -12,6 +25,8 @@ describe('Auth hook', () => {
       email: 'johndoe@example.com',
       password: '123456',
     });
+
+    await waitForNextUpdate();
 
     expect(result.current.user.email).toEqual('johndoe@example.com');
   });
